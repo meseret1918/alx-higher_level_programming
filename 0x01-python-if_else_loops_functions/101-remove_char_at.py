5d5231dc4b6c52d7c5b546bb7d782e09d968dfc4
meseret1918 <meseret1918@gmail.com>
2023-03-07 13:05:52 -0800

    Write a function that creates a copy of the string

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-i = 0
-cs = ""
-for char in str:
-if i != n:
-cs += char
-i += 1
-return (cs)
+    new = ''
+    i = 0
+    for c in str:
+        if i != n:
+            new += str[i]
+        i += 1
+    return new
