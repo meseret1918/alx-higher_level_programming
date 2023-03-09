a29905d2ce5610e66df7fcb4f2b454cdc9d26c50
meseret1918 <meseret1918@gmail.com>
2023-03-09 08:37:29 -0800

    Everything can be imported

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
+    import sys
     import hidden_4
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+    for i in dir(hidden_4):
+        if i[:2] != "__":
+            print(i)
+
