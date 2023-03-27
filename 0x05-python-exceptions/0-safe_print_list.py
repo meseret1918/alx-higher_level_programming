4483a2852e5949d09c55ede5c245b828ea97275e
meseret1918 <meseret1918@gmail.com>
2023-03-27 06:53:26 -0700

    Write a function that prints an integer

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    count = 0
-    for i in range(0, x):
+    ret = 0
+    for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
-            count += 1
-        except Exception:
-            continue
-        print()
-        return count
+            ret += 1
+        except IndexError:
+            break
+    print("")
+    return (ret)
