2206e97678d937c3a6193e684c9f2474b69c0f74
meseret1918 <meseret1918@gmail.com>
2023-04-05 10:57:33 -0700

    update

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,23 +1,39 @@
 #!/usr/bin/python3
+"""
+This is the matrix_divided module.
+This module supplies one function, matrix_divided().
+"""
+
+
 def matrix_divided(matrix, div):
-    message = "matrix must be a matrix (list of lists) of integers/floats"
-    if not matrix:
-        raise TypeError(message)
-    if not isinstance(matrix, list):
-        raise TypeError(message)
-    for lists in matrix:
-        if not isinstance(lists, list):
-            raise TypeError(message)
-        for item in lists:
-            if not isinstance(item, int) and not isinstance(item, float):
-                raise TypeError(message)
-    for lists in matrix:
-        if len(lists) == 0:
-            raise TypeError(message)
-    if not isinstance(div, int) and not isinstance(div, float):
-        raise TypeError("div must be a number")
-    if not all(len(lists) == len(matrix[0]) for lists in matrix):
-        raise TypeError("Each row of the matrix must have the same size")
-    if div == 0:
-        raise ZeroDivisionError("division by zero")
-    return [[round(item / div, 2) for item in lists] for lists in matrix]
+    """
+    Return a new matrix where each element has been divided by div.
+    Args:
+        matrix (list): list of lists of integers or floats.
+        div (int, float): the divider, >= 0.
+    """
+    mtrx_type_e = 'matrix must be a matrix (list of lists) of integers/floats'
+    mtrx_len_e = 'Each row of the matrix must have the same size'
+    div_type_e = 'div must be a number'
+    div_zero_e = 'division by zero'
+
+    row_len = 0
+    if type(matrix) is not list:
+        raise TypeError(mtrx_type_e)
+
+    for row in matrix:
+        if type(row) is not list:
+            raise TypeError(mtrx_type_e)
+        for i in row:
+            if type(i) not in [int, float]:
+                raise TypeError(mtrx_type_e)
+        if len(row) is not row_len and row_len is not 0:
+            raise TypeError(mtrx_len_e)
+        row_len = len(row)
+
+    if type(div) not in [int, float]:
+        raise TypeError(div_type_e)
+    if div is 0:
+        raise ZeroDivisionError(div_zero_e)
+
+    return [[round(nb / div, 2) for nb in row] for row in matrix]
--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 def print_square(size):
-    if not isinstance(size, int):
+    if type(size) != int:
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    for index in range(size):
-        print("x" * size)
+    if type(size) == float and size < 0:
+        raise TypeError("size must be an integer")
+    for i in range(size):
+        for j in range(size):
+            print(("#"), end="")
+        print()
