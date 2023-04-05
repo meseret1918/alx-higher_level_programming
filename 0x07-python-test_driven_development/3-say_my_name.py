f62fdefd3bf564eb6d476cd474bc5c3c274f60fd
meseret1918 <meseret1918@gmail.com>
2023-04-05 13:29:55 -0700

    2

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-""" Module say_my_name """
+# 3-say_my_name.py
+"""Defines a name-printing function."""
 
 
 def say_my_name(first_name, last_name=""):
-    """Prints a string with <first_name> and <last_name>."""
-    if type(first_name) != str:
+    """Print a name.
+    Args:
+        first_name (str): The first name to print.
+        last_name (str): The last name to print.
+    Raises:
+        TypeError: If either of first_name or last_name are not strings.
+    """
+    if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-
-    if type(last_name) != str:
+    if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-
-    print("My name is {:s} {:s}".format(first_name, last_name))
-
-    if __name__ == "__main__":
-        import doctest
-        doctest.testmod()
+    print("My name is {} {}".format(first_name, last_name))
