a0385e7bd5204263f8da20cf23c54be06928e172
meseret1918 <meseret1918@gmail.com>
2023-04-05 11:29:02 -0700

    2

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 def matrix_divided(matrix, div):
-    message = "matrix must be a matrix (list of lists) of integers/floats"
+    errorMessage = "matrix must be a matrix (list of lists) of integers/floats"
     if not matrix:
-        raise TypeError(message)
+        raise TypeError(errorMessage)
     if not isinstance(matrix, list):
-        raise TypeError(message)
+        raise TypeError(errorMessage)
     for lists in matrix:
         if not isinstance(lists, list):
-            raise TypeError(message)
+            raise TypeError(errorMessage)
         for item in lists:
             if not isinstance(item, int) and not isinstance(item, float):
-                raise TypeError(message)
+                raise TypeError(errorMessage)
     for lists in matrix:
         if len(lists) == 0:
-            raise TypeError(message)
+            raise TypeError(errorMessage)
     if not isinstance(div, int) and not isinstance(div, float):
         raise TypeError("div must be a number")
     if not all(len(lists) == len(matrix[0]) for lists in matrix):
