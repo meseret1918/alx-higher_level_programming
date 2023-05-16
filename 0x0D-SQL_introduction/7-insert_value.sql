264d6fa900afab2b8058228ed1d8c0a7c1d32d47
meseret1918 <meseret1918@gmail.com>
2023-05-16 09:41:58 -0700

    SQL

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 -- Insert values
 -- Execute: cat 7-insert_value.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "My School");
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
