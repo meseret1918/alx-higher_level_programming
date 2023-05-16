b2d274c17ae8cb498bfc2a6cad6581338a40f9f4
meseret1918 <meseret1918@gmail.com>
2023-05-16 09:51:45 -0700

    updatt addvanced

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,7 @@
--- Change character SET and COLLATE
--- cat 100-move_to_utf8.sql | mysql -hlocalhost -uroot -p
-ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
-USE hbtn_0c_0
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table CHANGE name name VARCHAR (256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+#!/usr/bin/env bash
+-- converts hbtn_0c_0 database to UTF8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE hbtn_0c_0;
+-- convert first_table to UTF8
+ALTER TABLE first_table
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
