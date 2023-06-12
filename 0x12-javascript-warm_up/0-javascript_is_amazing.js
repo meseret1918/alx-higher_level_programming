825e4f3198fea33ce1d2477beb8df002f75dca1e
meseret1918 <meseret1918@gmail.com>
2023-06-12 04:52:33 -0700

    javascript

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,5 +1,3 @@
 #!/usr/bin/node
-// Print string “Javascript is amazing”
-
 const myVar = "Javascript is amazing";
 console.log(myVar);
--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,10 +1,2 @@
 #!/usr/bin/node
-// Write a script that prints 3 lines:
-//The first line: “C is fun”
-//The second line: “Python is cool”
-//The third line: “JavaScript is amazing”
-
-const myVar = "C is fun";
-const myVar = "Python is cool";
-const myVar = "Javascript is amazing";
-console.log(myVar);
+console.log("C is fun\nPython is cool\nJavascript is amazing");
