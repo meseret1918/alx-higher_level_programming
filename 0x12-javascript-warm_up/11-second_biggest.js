957a17cfdc7609c3f3fe321744b64f52e3925b18
meseret1918 <meseret1918@gmail.com>
2023-06-12 07:08:56 -0700

    javascript

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,6 +2,8 @@
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const list = process.argv.sort();
-  console.log(list.reverse()[1]);
+  const args = process.argv.map(Number)
+    .slice(2, process.argv.length)
+    .sort((a, b) => a - b);
+  console.log(args[args.length - 2]);
 }
---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,2 @@
+#!/usr/bin/node
+exports.add = (intg1, intg2) => intg1 + intg2;
