d6a7bd8d4d1bdf34fb065f270812e678971d6bff
meseret1918 <meseret1918@gmail.com>
2023-06-12 06:51:27 -0700

    javascript

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < langs.length; i++) {
+const langs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i;
+for (i = 0; i < langs.length; i++) {
   console.log(langs[i]);
 }
