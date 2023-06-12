b6616da412e95bbc1571acc67b7b96d4635e1e7f
meseret1918 <meseret1918@gmail.com>
2023-06-12 06:37:39 -0700

    javascript

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,3 @@
 #!/usr/bin/node
-console.log('C is fun');
-console.log('Python is cool');
-console.log('Javascript is amazing');
+const language = ('C is fun\n' + 'Python is cool\n' + 'Javascript is amazing');
+console.log(language);
