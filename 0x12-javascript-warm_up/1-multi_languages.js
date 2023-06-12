90f011ffbd62450ffd88d40ca8fa8336dd82260e
meseret1918 <meseret1918@gmail.com>
2023-06-12 04:32:16 -0700

    javascript

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Write a script that prints 3 lines:
+//The first line: “C is fun”
+//The second line: “Python is cool”
+//The third line: “JavaScript is amazing”
+
+const myVar = "C is fun";
+const myVar = "Python is cool";
+const myVar = "Javascript is amazing";
+console.log(myVar);
