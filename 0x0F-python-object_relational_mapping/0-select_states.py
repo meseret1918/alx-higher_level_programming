f498909aee3631c228fa723756cf01ddcb4486ff
meseret1918 <meseret1918@gmail.com>
2023-06-16 02:08:40 -0700

    python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
-#Write a script that lists all states from the database hbtn_0e_0_usa:
-
-#Your script should take 3 arguments: mysql username, mysql password and database name (no argument validation needed)
-#You must use the module MySQLdb (import MySQLdb)
-#Your script should connect to a MySQL server running on localhost at port 3306
-#Results must be sorted in ascending order by states.id
-#Results must be displayed as they are in the example below
-#Your code should not be executed when imported
-import sys
+""" Script that lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
+from sys import argv
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states`")
-    [print(state) for state in c.fetchall()]
+# The code should not be executed when imported
+if __name__ == '__main__':
+
+    # make a connection to the database
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+
+    # It gives us the ability to have multiple seperate working environments
+    # through the same connection to the database.
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
+    # Clean up process
+    cur.close()
+    db.close()
