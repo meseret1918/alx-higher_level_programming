bba32ae53f1e5796f745d83dafe0c24a8b0d4180
meseret1918 <meseret1918@gmail.com>
2023-06-30 06:32:35 -0700

    0x11-python-network_1

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-"""Python script that takes in a letter and sends a POST request to
-http://0.0.0.0:5000/search_user with the letter as a parameter."""
+"""displays the value of the X-Request-Id variable found in
+the header of the response.
+"""
 
-import requests
-from sys import argv
 
 if __name__ == "__main__":
-    data = {"q": argv[1] if len(argv) > 1 else ""}
-    request = requests.post("http://0.0.0.0:5000/search_user", data=data)
+    from requests import post
+    from sys import argv
+
+    q = argv[1] if len(argv) > 1 else ""
+    r = post('http://0.0.0.0:5000/search_user', data={'q': q})
     try:
-        json = request.json()
-        if json:
-            print("[{}] {}".format(json.get("id"), json.get("name")))
+        response = r.json()
+        if response == {}:
+            print('No result')
         else:
-            print("No result")
-    except:
-        print("Not a valid JSON")
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print('Not a valid JSON')
