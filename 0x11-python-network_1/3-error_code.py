78f845bc36be4445acf5863f7d72ecdf565e303b
meseret1918 <meseret1918@gmail.com>
2023-06-30 06:29:00 -0700

    0x11-python-network_1

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL, sends a request to the URL
-and displays the body of the response (decoded in utf-8)."""
+"""displays the value of the X-Request-Id variable found in
+the header of the response.
+"""
 
-from urllib import request, error
-from sys import argv
 
 if __name__ == "__main__":
-        try:
-            with request.urlopen(argv[1]) as page:
-                print(page.read().decode('utf-8'))
-        except error.HTTPError as e:
-            print("Error code: {}".format(e.code))
+    from urllib.request import urlopen
+    from urllib.error import HTTPError
+    from sys import argv
+
+    try:
+        with urlopen(argv[1]) as response:
+            print(response.read().decode('utf-8'))
+    except HTTPError as error:
+        print('Error code:', error.code)
