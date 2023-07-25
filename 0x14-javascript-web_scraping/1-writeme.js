005824f532fa84d849a969de31a811a31220ca21
meseret1918 <meseret1918@gmail.com>
2023-07-25 13:21:29 +0000

    javascript-web_scraping

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,13 +2,8 @@
 
 const fs = require('fs');
 
-const filePath = process.argv[2];
-const content = process.argv[3];
-
-fs.writeFile(filePath, content, 'utf-8', (err) => {
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err) {
   if (err) {
-    console.error(err);
-  } else {
-    console.log(`Content has been written to ${filePath}`);
+    console.log(err);
   }
 });
