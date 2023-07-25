a448fd5607de918c4c8864d0134d7c3860412af6
meseret1918 <meseret1918@gmail.com>
2023-07-25 14:43:11 +0000

    javascript-web_scraping

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,14 +1,15 @@
-#!/usr/bin/node
-
 const request = require('request');
 
 const apiUrl = process.argv[2];
 
 request(apiUrl, (error, response, body) => {
   if (error) {
-    console.error(error);
+    console.error('Error:', error);
+  } else if (response.statusCode !== 200) {
+    console.error('Status:', response.statusCode);
   } else {
     const todos = JSON.parse(body);
+
     const completedTasks = {};
 
     todos.forEach((todo) => {
