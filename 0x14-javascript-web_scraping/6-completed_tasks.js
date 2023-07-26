b9c2dd3f91797fe21c24d87560793bf4cd40a918
meseret1918 <meseret1918@gmail.com>
2023-07-26 07:32:19 +0000

    javascript

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,17 +1,17 @@
 #!/usr/bin/node
 
 const request = require('request');
-
 request(process.argv[2], function (err, resp, body) {
   if (!err) {
     const data = JSON.parse(body);
-    let checkers = 0;
+    const completedUsers = {};
     data.forEach((element) => {
-      if (element.completed) {
-        checkers++;
+      if (element.completed && completedUsers[element.userId] === undefined) {
+        completedUsers[element.userId] = 1;
+      } else if (element.completed) {
+        completedUsers[element.userId] += 1;
       }
     });
-
-    console.log(`Total Checkers: ${checkers}`);
+    console.log(completedUsers);
   }
 });
