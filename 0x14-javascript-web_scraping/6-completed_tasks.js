862140396bfa3d6e261deb967afa3381d8794146
meseret1918 <meseret1918@gmail.com>
2023-07-26 07:17:26 +0000

    javascript

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,26 +1,16 @@
 #!/usr/bin/node
-// A script that gets the contents of a webpage and stores it in a file
-
 const request = require('request');
-const url = process.argv[2];
-
-const dictList = {};
-
-request(url, { json: true }, (err, res) => {
-  const list = res.body;
-  if (err) {
-    console.log(err);
-  } else {
-    for (let i = 0; i < list.length; i++) {
-      const key = list[i].userId;
-      if (list[i].completed === true) {
-        if (!dictList[key]) {
-          dictList[key] = 1;
-        } else {
-          dictList[key] += 1;
-        }
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const todos = JSON.parse(body);
+    let completed = {};
+    todos.forEach((todo) => {
+      if (todo.completed && completed[todo.userId] === undefined) {
+        completed[todo.userId] = 1;
+      } else if (todo.completed) {
+        completed[todo.userId] += 1;
       }
-    }
-    console.log(dictList);
+    });
+    console.log(completed);
   }
 });
